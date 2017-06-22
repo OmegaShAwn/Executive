5ccab8aaae60371671490a44726dc93e1a6b49cb
ShAwn <shawnsthottan@gmail.com>
2017-06-22 10:47:46 +0530

    number of casualities

--- app/src/main/java/com/example/android/executive/EmergencyDetails.java
+++ app/src/main/java/com/example/android/executive/EmergencyDetails.java
@@ -8,23 +8,25 @@ public class EmergencyDetails {
 
     public String si;
     public String ti;
+    public String no;
     public String username;
 
     public EmergencyDetails(){}
 
-    public EmergencyDetails(String si,String ti,String username){
+    public EmergencyDetails(String si,String ti,String username,String no){
         this.si=si;
         this.ti=ti;
         this.username=username;
     }
 
-    public void setSi(String si){this.si=si;    }
+    public void setSi(String si){this.si=si;}
     public void setTi(String ti){this.ti=ti;}
     public String getSi(){
         if(si==null)
             si="0";
         return si;
     }
+    public String getno(){return no;}
     public String getTi(){return ti;}
     public void setUsername(String username){this.username=username;}
     public String getUsername(){return username;}
